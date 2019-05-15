0bab0f6e63d80e94b7b547f345dc5e8c35385d83
Gaurab KC <35660893+kecyGaurab@users.noreply.github.com>
2019-05-15 12:18:41 +0300

    function to calculate distance with coordiinates

--- src/test.js
+++ src/test.js
@@ -12,3 +12,25 @@ function getLocation() {
     x.innerHTML = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;
   }
+
+  // calculate distance
+function distance(lat1, lon1, lat2, lon2) {
+	if ((lat1 === lat2) && (lon1 === lon2)) {
+		return 0;
+	} else {
+		let radlat1 = Math.PI * lat1 / 180,
+			radlat2 = Math.PI * lat2 / 180,
+			theta = lon1 - lon2,
+			radtheta = Math.PI * theta / 180,
+			dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+
+		dist = dist > 1 ? 1 : dist;
+		dist = Math.acos(dist);
+		dist = dist * 180 / Math.PI;
+		dist = dist * 60 * 1.1515;
+		dist = dist * 1.609344
+
+		return dist;
+	}
+
+}
