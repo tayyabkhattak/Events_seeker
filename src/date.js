4c040ae4d29e7d937c12908fd50df5647155b995
Bishnu Kharel <bishnu88@hotmail.com>
2019-05-15 19:22:33 +0300

    UI ready for

---
+++ src/date.js
@@ -0,0 +1,5 @@
+let dateOption =  document.querySelector('#date-option');
+let now = new Date();
+let fullDate = now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + now.getDate();
+console.log(fullDate);
+// loop 3 times
